61e59e4467e4b3d14f49a545f8f6509b53f8739f
Alaa <soupermado@gmail.com>
2022-10-13 22:49:13 +0200

    👨‍🔧 add missing data to terraform state in domain resource

--- postmark/resource_domain.go
+++ postmark/resource_domain.go
@@ -21,9 +21,28 @@ func resourceDomain() *schema.Resource {
 				Required: true,
 			},
 			"return_path_domain": &schema.Schema{
-				Type:     schema.TypeString,
-				Computed: true,
-				Optional: true,
+				Type:      schema.TypeString,
+				Computed:  true,
+				Optional:  true,
+				Sensitive: true,
+			},
+			"dkim_pending_text_value": &schema.Schema{
+				Type:      schema.TypeString,
+				Computed:  true,
+				Optional:  true,
+				Sensitive: true,
+			},
+			"return_path_domain_cname_value": &schema.Schema{
+				Type:      schema.TypeString,
+				Computed:  true,
+				Optional:  true,
+				Sensitive: true,
+			},
+			"dkim_pending_host": &schema.Schema{
+				Type:      schema.TypeString,
+				Computed:  true,
+				Optional:  true,
+				Sensitive: true,
 			},
 			"last_updated": &schema.Schema{
 				Type:     schema.TypeString,
@@ -35,9 +54,12 @@ func resourceDomain() *schema.Resource {
 }
 
 type Domain struct {
-	Name             string `json:"Name"`
-	ID               int64  `json:"ID"`
-	ReturnPathDomain string `json:"ReturnPathDomain"`
+	Name                       string `json:"Name"`
+	ID                         int64  `json:"ID"`
+	DKIMPendingTextValue       string `json:"DKIMPendingTextValue"`
+	DKIMPendingHost            string `json:"DKIMPendingHost"`
+	ReturnPathDomainCNAMEValue string `json:"ReturnPathDomainCNAMEValue"`
+	ReturnPathDomain           string `json:"ReturnPathDomain"`
 }
 
 func resourceDomainCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
@@ -51,6 +73,24 @@ func resourceDomainCreate(ctx context.Context, d *schema.ResourceData, m interfa
 		return diags
 	}
 	d.SetId(strconv.FormatInt(domain.ID, 10))
+	if err := d.Set("dkim_pending_text_value", domain.DKIMPendingTextValue); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("dkim_pending_host", domain.DKIMPendingHost); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("return_path_domain_cname_value", domain.ReturnPathDomainCNAMEValue); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if domain.ReturnPathDomain == "" {
+		domain.ReturnPathDomain = "pm-bounces"
+	}
+	if err := d.Set("return_path_domain", domain.ReturnPathDomain); err != nil {
+		return diag.FromErr(err)
+	}
 
 	return diags
 }
@@ -79,6 +119,22 @@ func resourceDomainRead(ctx context.Context, d *schema.ResourceData, m interface
 	}
 	d.SetId(strconv.FormatInt(domain.ID, 10))
 	if err := d.Set("name", domain.Name); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("dkim_pending_text_value", domain.DKIMPendingTextValue); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("dkim_pending_host", domain.DKIMPendingHost); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("return_path_domain_cname_value", domain.ReturnPathDomainCNAMEValue); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("return_path_domain", domain.ReturnPathDomain); err != nil {
 		return diag.FromErr(err)
 	}
 
